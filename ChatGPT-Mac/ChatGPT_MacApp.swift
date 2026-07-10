b0674812c1360e59929a9296e28ea74485fbfea1
DEmpire412 <105176197+DEmpire412@users.noreply.github.com>
2026-07-09 23:54:54 -0400

    Initial Commit

---
+++ ChatGPT-Mac/ChatGPT_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_MacApp.swift
+//  ChatGPT-Mac
+//
+//  Created by David Empire  on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
